077c6a328d92ed9f668d06f3d8d099663a0e9cb8
Peter Vrancx <peter@prowler.io>
2019-12-20 16:05:05 +0000

    day 20

--- solutions/day20.py
+++ solutions/day20.py
@@ -1,22 +1,191 @@
 import numpy as np
 
-from src.graphs import Graph, Position
+from src.graphs import Graph, Position, dijkstra, Edge, AbstractGraph
 
 
 def valid_pos(pos, grid):
     n_rows, n_cols = grid.shape
-    return 0 <= pos.x < n_cols and 0<= pos.y < n_rows
+    return 0 <= pos.x < n_cols and 0 <= pos.y < n_rows
 
 
-def neighbours(pos, grid):
+def portal_pos(pos, grid):
+    return 65 <= grid[pos.y, pos.x] <= 90
+
+
+def maze_pos(pos, grid):
+    return grid[pos.y, pos.x] == ord('.')
+
+
+def horizontal_portal(p1, p2):
+    return p1.y != p2.py and p1.x == p2.x
+
+
+def vertical_portal(p1, p2):
+    return p1.y == p2.py and p1.x != p2.x
+
+
+def get_neighbours(pos, grid):
+    neighb = [Position(pos.x+1, pos.y),
+              Position(pos.x, pos.y+1),
+              Position(pos.x-1, pos.y),
+              Position(pos.x, pos.y-1)]
+    return [n for n in neighb if valid_pos(n, grid) and maze_pos(n, grid)]
+
+
+def get_portal_id(p1, p2, grid):
+    return chr(grid[p1.y, p1.x]) + chr(grid[p2.y, p2.x])
+
+
+def scan_h_portals(grid, portals):
+    for row_id in range(0,grid.shape[0], 2):
+        row = grid[row_id]
+        portal_idx = np.where(is_portal(row))[0]
+
+        for col_id in portal_idx:
+            up = Position(col_id, row_id-1)
+            down = Position(col_id, row_id+1)
+            if valid_pos(up, grid) and portal_pos(up, grid):
+                portal_id = get_portal_id(up, Position(col_id, row_id), grid)
+                if valid_pos(down, grid) and maze_pos(down, grid):
+                    portal_entrance = down
+                else:
+                    portal_entrance = Position(col_id, row_id-2)
+                if portal_id in portals:
+                    portals[portal_id].append(portal_entrance)
+                else:
+                    portals[portal_id] = [portal_entrance]
+            elif valid_pos(down, grid) and portal_pos(down, grid):
+                portal_id = get_portal_id(Position(col_id, row_id), down, grid)
+                if valid_pos(up, grid) and maze_pos(up, grid):
+                    portal_entrance = up
+                else:
+                    portal_entrance = Position(col_id, row_id+2)
+                if portal_id in portals:
+                    portals[portal_id].append(portal_entrance)
+                else:
+                    portals[portal_id] = [portal_entrance]
+    return portals
+
+
+def scan_v_portals(grid, portals):
+    for col_id in range(0, grid.shape[1], 2):
+        col = grid[:, col_id]
+        portal_idx = np.where(is_portal(col))[0]
+
+        for row_id in portal_idx:
+            left = Position(col_id-1, row_id)
+            right = Position(col_id+1, row_id)
+            if valid_pos(left, grid) and portal_pos(left, grid):
+                portal_id = get_portal_id(left, Position(col_id, row_id), grid)
+                if valid_pos(right, grid) and maze_pos(right, grid):
+                    portal_entrance = right
+                else:
+                    portal_entrance = Position(col_id-2, row_id)
+                if portal_id in portals:
+                    portals[portal_id].append(portal_entrance)
+                else:
+                    portals[portal_id] = [portal_entrance]
+            elif valid_pos(right, grid) and portal_pos(right, grid):
+                portal_id = get_portal_id(Position(col_id, row_id), right, grid)
+                if valid_pos(left, grid) and maze_pos(left, grid):
+                    portal_entrance = left
+                else:
+                    portal_entrance = Position(col_id+2, row_id)
+                if portal_id in portals:
+                    portals[portal_id].append(portal_entrance)
+                else:
+                    portals[portal_id] = [portal_entrance]
+    return portals
+
+
+def get_portals(grid):
+    portals = scan_h_portals(grid, {})
+    return scan_v_portals(grid, portals)
+
+
+def is_portal(grid):
+    return np.logical_and(grid >= 65, grid <= 90)
+
+
+def readmap(filename):
     result = []
-    neighb = [Position(pos.x+1, pos.y)
-
+    with open(filename, 'r') as f:
+        for line in f:
+            result.append([ord(ch) for ch in line])
+    return np.array(result)
 
 
 def build_graph(grid):
     graph = Graph()
     ys, xs = np.where(grid == ord('.'))
+    portals = get_portals(grid)
+
     for x, y in zip(xs, ys):
-        graph.add_node(Position(x, y))
+        pos = Position(x, y)
+        graph.add_node(pos)
+
+    for pos in graph.get_nodes():
+        neighbours = get_neighbours(pos, grid)
+        for n in neighbours:
+            graph.add_edge(pos, n)
+
+    for p_id, route in portals.items():
+        if p_id not in ['AA', 'ZZ']:
+            graph.add_edge(route[0], route[1], info='portal')
+            graph.add_edge(route[1], route[0], info='portal')
+
+    return graph, portals
+
+
+class RecursiveGraph(AbstractGraph):
+    def __init__(self, graph: Graph):
+        self._graph = graph
+        coords = np.array([[n.x, n.y] for n in graph.get_nodes()])
+        self.minx, self.miny = np.min(coords, axis=0)
+        self.maxx, self.maxy = np.max(coords, axis=0)
+
+
+    def _is_outer_portal(self, pos):
+        return pos.x == self.minx or \
+               pos.x == self.maxx or \
+               pos.y == self.miny or \
+               pos.y == self.maxy
+
+    def get_edges(self, node):
+
+        level, pos = node
+        result = []
+
+        edges = self._graph.get_edges(pos)
+        for edge in edges:
+            if edge.info == 'portal':
+                if self._is_outer_portal(pos):
+                    new_level = level - 1
+                else:
+                    new_level = level + 1
+                if new_level >= 0:
+                    result.append(Edge(node, (new_level, edge.target), edge.weight, edge.info))
+            else:
+                result.append(Edge(node, (level, edge.target), edge.weight, edge.info))
+        return result
+
+
+if __name__=='__main__':
+    def _main():
+        grid = readmap('../inputs/day20.txt')
+        graph, portals = build_graph(grid)
+
+        start = portals['AA'][0]
+        goal = portals['ZZ'][0]
+
+        result = dijkstra(graph, start, lambda n: n == goal)
+        print(result.cost)
+
+        rgraph = RecursiveGraph(graph)
+
+        result = dijkstra(rgraph, (0, start), lambda n: n == (0, goal))
+        print(result.cost)
+
+    _main()
+
 
--- src/graphs.py
+++ src/graphs.py
@@ -1,4 +1,7 @@
-from typing import Hashable, NamedTuple, List
+import abc
+from collections import deque
+import numpy as np
+from typing import Hashable, NamedTuple, List, Any, Callable, Tuple
 
 Node = Hashable
 
@@ -15,7 +18,16 @@ class Edge(NamedTuple):
     info: Hashable
 
 
-class Graph:
+class AbstractGraph(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def get_edges(self, node: Node) -> List[Edge]:
+        raise NotImplementedError()
+
+    def get_neighbours(self, node: Node) -> List[Node]:
+        return [edge.target for edge in self.get_edges(node)]
+
+
+class Graph(AbstractGraph):
     def __init__(self):
         self._nodes = set()
         self._edges = {}
@@ -24,8 +36,9 @@ class Graph:
         return node in self._nodes
 
     def add_node(self, node: Node):
-        self._nodes.add(node)
-        self._edges[node] = []
+        if node not in self._nodes:
+            self._nodes.add(node)
+            self._edges[node] = []
 
     def add_edge(self, node1: Node, node2: Node, weight: float=1., info=None):
         assert self.is_valid_node(node1)
@@ -39,5 +52,123 @@ class Graph:
     def get_edges(self, node: Node) -> List[Edge]:
         return self._edges[node]
 
-    def get_neighbours(self, node):
+    def get_neighbours(self, node: Node) -> List[Node]:
         return [edge.target for edge in self.get_edges(node)]
+
+
+class Agenda(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def next_item(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def add_item(self, item: Any):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def clear(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def items(self):
+        raise NotImplementedError()
+
+    def __len__(self):
+        return 0
+
+
+class QueueAgenda(Agenda):
+    def __init__(self):
+        self._queue = deque()
+
+    def next_item(self):
+        return self._queue.popleft()
+
+    def add_item(self, item: Any):
+        self._queue.append(item)
+
+    def clear(self):
+        self._queue.clear()
+
+    def items(self):
+        return list(self._queue)
+
+    def remove(self, item):
+        self._queue.remove(item)
+
+    def __len__(self):
+        return len(self._queue)
+
+
+class StackAgenda(QueueAgenda):
+    def next_item(self):
+        return self._queue.pop()
+
+
+class SearchNode(NamedTuple):
+    state: Node
+    cost: float
+    path: Tuple
+
+
+def search(graph: AbstractGraph, start: Node, agenda: Agenda, goal_fn: Callable):
+    agenda.add_item(SearchNode(start, 0., ()))
+    visited = set()
+    while len(agenda) > 0:
+        next_node = agenda.next_item()
+        if goal_fn(next_node):
+                return next_node
+        visited.add(next_node.state)
+        edges = graph.get_edges(next_node.state)
+        for edge in edges:
+            if edge.target not in visited:
+                agenda.add_item(SearchNode(edge.target,
+                                           next_node.cost + edge.weight,
+                                           next_node.path + (next_node.state,)))
+    return None
+
+
+def bfs(graph: AbstractGraph, start: Node, goal_fn: Callable):
+    return search(graph, start, QueueAgenda(), goal_fn)
+
+
+def dfs(graph: AbstractGraph, start: Node, goal_fn: Callable):
+    return search(graph, start, StackAgenda(), goal_fn)
+
+
+def dijkstra(graph: AbstractGraph, start: Node, goal_fn: Callable):
+    agenda = QueueAgenda()  # basic list impl rather than PQ
+
+    start_node = SearchNode(start, 0., ())
+    agenda.add_item(start_node)
+    dists = {start: 0.}
+    visited = set()
+
+    while len(agenda) > 0:
+        min_dist = np.inf
+        next_node = None
+
+        for n in agenda.items():
+            if dists[n.state] < min_dist and n.state not in visited:
+                min_dist = dists[n.state]
+                next_node = n
+        agenda.remove(next_node)
+        visited.add(next_node.state)
+
+        if goal_fn(next_node.state):
+            return next_node
+
+        edges = graph.get_edges(next_node.state)
+
+        for edge in edges:
+            node_dist = dists[edge.target] if edge.target in dists else np.inf
+            tot_dist = edge.weight + min_dist
+            if tot_dist < node_dist:
+                node = SearchNode(
+                    edge.target,
+                    tot_dist,
+                    next_node.path + (next_node.state,))
+                dists[edge.target] = tot_dist
+                agenda.add_item(node)
+    return None
+
